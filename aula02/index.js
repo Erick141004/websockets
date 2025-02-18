cdd497a73895ac685a2b71fbdfe62545b436f8cc
ErickLima <erickcreimberg@gmail.com>
2025-02-18 13:26:34 -0300

    Exercicio aula 2

--- aula02/index.js
+++ aula02/index.js
@@ -1,7 +1,9 @@
-import { server } from './server.js';
+import { server } from "./server.js";
 
 const PORT = 8000;
 
-server.listen(PORT, '127.0.0.1', () => {
-    console.log('Server is running...')
-})
+export const users = [];
+
+server.listen(PORT, "127.0.0.1", () => {
+  console.log("Server is running...");
+});
--- aula02/server.js
+++ aula02/server.js
@@ -1,47 +1,59 @@
-import { createServer } from "http"
-import { URL } from 'url';
+import { createServer } from "http";
+import { URL } from "url";
+import { users } from "./index.js"; // users agora é compartilhado
 
 export const server = createServer((req, res) => {
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    const path = url.pathname;
-    const users = []
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const path = url.pathname;
 
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
 
-    switch(path){
-        case '/addUser':
-            res.writeHead(200);
-            const teste = getUser();
-            users.push(teste);
-            res.end(`Deu algum bom ai${users}`)
-            break;
-        case '/users':
-            res.writeHead(200);
-            break;
-        case '/user':
-            res.writeHead(200);
-            break;
-        case '/':
-            res.writeHead(200);
-            res.end("Deu bom porra");
-            break;
-        default:
-            res.writeHead(404);
-            break;
+  if (path.startsWith("/user/")) {
+    const id = parseInt(path.split("/")[2], 10);
+
+    if (isNaN(id) || id < 0 || id >= users.length) {
+      res.writeHead(404);
+      return res.end(JSON.stringify({ error: "Usuário não encontrado" }));
     }
-})
 
-function getUser(){
-    fetch("https://randomuser.me/api/", {method: "GET"})
-    .then((response) => {
-        return response.json()
-    })
-    .then(
-        (response) => {
-            return response.results[0]
-        }
-    )
+    res.writeHead(200);
+    return res.end(JSON.stringify(users[id]));
+  }
+
+  switch (path) {
+    case "/addUser":
+      res.writeHead(200);
+      getUser()
+        .then((value) => {
+          users.push(value);
+          console.log(users);
+          res.end(`Usuário adicionado!`);
+        })
+        .catch((e) => {
+          res.end(`Erro ao adicionar usuário: ${e}`);
+        });
+      break;
+    case "/users":
+      res.writeHead(200);
+      res.end(JSON.stringify(users, null, 2));
+      break;
+    case "/":
+      res.writeHead(200);
+      res.end("Deu bom");
+      break;
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: "Rota não encontrada" }));
+      break;
+  }
+});
+
+function getUser() {
+  return fetch("https://randomuser.me/api/")
+    .then((response) => response.json())
+    .then((data) => data.results[0])
     .catch((e) => {
-        console.log(`Erro na seguinte coisa ${e}`)
-    })
+      console.error(`Erro ao buscar usuário: ${e}`);
+      throw e;
+    });
 }
